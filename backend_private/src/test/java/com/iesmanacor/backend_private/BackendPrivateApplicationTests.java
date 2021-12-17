2d5793512fc364da11c8b7762acfbf97bf26a156
Aleixfo <fontcomesaleix@gmail.com>
2021-12-17 12:02:39 +0100

    primerpush

---
+++ backend_private/src/test/java/com/iesmanacor/backend_private/BackendPrivateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesmanacor.backend_private;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPrivateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
